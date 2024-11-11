3ec41bf9066e358f23ae892f0e501a8794540323
Sebghatullah Amin <95332899+SebghatAmin@users.noreply.github.com>
2024-11-12 02:11:31 +0430

    Header section complited on 1:17:42 time

---
+++ src/assets/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReviews,
+  Footer,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Services,
+};
