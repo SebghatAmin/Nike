7d6c5b853bc565c19f8835e6607842c10a15ec14
Office Laptop <sebghatullah1994@gmail.com>
2024-11-12 18:17:51 +0430

    40

--- src/assets/sections/Hero.jsx
+++ src/assets/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "../../components/Button";
+import ShoeCard from "../../components/ShoeCard";
 import { statistics } from "../../constants";
 import { arrowRight } from "../icons";
+import { bigShoe1 } from "../images";
 
 const Hero = () => {
   return (
@@ -22,10 +24,18 @@ const Hero = () => {
 
               </div>))
           }
-        </div>
-      </div>
-    </section>
-    
+        </div>       
+      </div>   
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen">
+      <img src={bigShoe1}
+      alt="Shoe Collection"
+      width={610}
+      height={500}
+      className="object-contain relative z-10"
+      />
+
+      </div>   
+    </section>    
   );
 };
 
