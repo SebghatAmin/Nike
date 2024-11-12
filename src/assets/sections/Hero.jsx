ec13611e34689eba74301b0776b8d31d5ce52384
Office Laptop <sebghatullah1994@gmail.com>
2024-11-12 11:29:24 +0430

    min 26 done

--- src/assets/sections/Hero.jsx
+++ src/assets/sections/Hero.jsx
@@ -1,11 +1,20 @@
+import Button from "../../components/Button";
+import { arrowRight } from "../icons";
+
 const Hero = () => {
   return (
     <section
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl-w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28"></div>
+      <div className="relative xl-w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <p>Our Sumer Collection</p>
+      <h1><span>The New Arrival
+        </span><br/><span>Nike</span>Shoes</h1>
+        <p>Discover styles Nike Arrivals,quality comfort,and innovation for your active life. </p>
+        <Button label="Shop now" iconURL={arrowRight}/></div>
     </section>
+    
   );
 };
 
