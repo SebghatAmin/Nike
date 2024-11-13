851037c26000e3e0bf5df0ffc13a85afd778dd69
Sebghatullah Amin <95332899+SebghatAmin@users.noreply.github.com>
2024-11-13 12:48:35 +0430

    prob fixed

--- src/assets/sections/Hero.jsx
+++ src/assets/sections/Hero.jsx
@@ -10,26 +10,26 @@ const Hero = () => {
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl-w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      {/* Left Column */}
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full px-4 pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Sumer Collection
+          Our Summer Collection
         </p>
-        <h1 className="mt-10 font-sans text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-white  xl:whitespace-nowrap relative z-10 pr-10">
+        <h1 className="mt-10 font-sans text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
           <br />
           <span className="text-coral-red mt-3 inline-block">Nike</span>Shoes
         </h1>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm-max-w-sm">
-          Discover styles Nike Arrivals,quality comfort,and innovation for your
-          active life.{" "}
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover styles, quality comfort, and innovation for your active life.
         </p>
         <Button label="Shop now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p className="text-4xl font-montserrat font-bold">{stat.value}</p>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
               </p>
@@ -37,6 +37,8 @@ const Hero = () => {
           ))}
         </div>
       </div>
+
+      {/* Right Column */}
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
           src={bigShoe1}
