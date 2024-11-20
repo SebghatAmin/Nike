6aaeb700dfdf38f04398dac4f1874060bd43c52c
Office Laptop <sebghatullah1994@gmail.com>
2024-11-20 16:33:55 +0430

    Customer Rewiew done

--- src/assets/sections/CustomerReviews.jsx
+++ src/assets/sections/CustomerReviews.jsx
@@ -1,7 +1,31 @@
+import { reviews } from "../../constants";
+import ReviewCard from "../../components/ReviewCard";
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
 
-export default CustomerReviews
+      <p className="text-info m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experience with us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((reviews) => (
+          <ReviewCard
+            key={reviews.customerName}
+            imgURL={reviews.imgURL}
+            customerName={reviews.customerName}
+            rating={reviews.rating}
+            feedback={reviews.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CustomerReviews;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,31 @@
-const Button = ({ label, iconURL,backgroundColor,textColor,borderColor }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-    ${backgroundColor
-        ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red  text-white border-coral-red} rounded-full"`}>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red  text-white border-coral-red"
+      } rounded-full w-full"
+      }
+    `}
+    >
       {label}
-      {iconURL && <  img
-        src={iconURL}
-        alt="Arrow Right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="Arrow Right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
 
 export default Button;
-`} />)
-`} />);
-}
